37f5972c8465ea984cc17d9064bbb0f9b8350ab8
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-10-23 23:56:59 +0530

    updating

--- case_NumberToWord.js
+++ case_NumberToWord.js
@@ -2,22 +2,22 @@ const digit = 100;
 console.log("The digit is: " + digit);
 
 switch (digit) {
-    case "1":
+    case 1:
         console.log("Unit");
       break;
-    case "10":
+    case 10:
         console.log("Ten");
       break;
-    case "100":
+    case 100:
         console.log("Hundred");
       break;
-    case "1000":
+    case 1000:
         console.log("Thousand");
       break;
-    case "10,000":
+    case 10,000:
         console.log("Ten Thousand");
       break;
-    case "1,00,000":
+    case 1,00,000:
         console.log("One lakh");
       break;
     default:
