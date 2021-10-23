1bba53ddc12ccc13a168d83423c8b03374057ca7
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-10-23 23:55:31 +0530

    number to words by switch program

---
+++ case_NumberToWord.js
@@ -0,0 +1,26 @@
+const digit = 100;
+console.log("The digit is: " + digit);
+
+switch (digit) {
+    case "1":
+        console.log("Unit");
+      break;
+    case "10":
+        console.log("Ten");
+      break;
+    case "100":
+        console.log("Hundred");
+      break;
+    case "1000":
+        console.log("Thousand");
+      break;
+    case "10,000":
+        console.log("Ten Thousand");
+      break;
+    case "1,00,000":
+        console.log("One lakh");
+      break;
+    default:
+        console.log("Invalid Option");
+      break;
+  }
