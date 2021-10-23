10c4ffb97388340dd62c59cd559ab593779d8ff5
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-10-24 00:04:45 +0530

    check prime number

---
+++ CheckPrime.js
@@ -0,0 +1,12 @@
+let num= 5;
+flag=0
+      for (let i=2; i <= num/2; i++ )
+        {
+          if ( num % i == 0 )
+               flag=1 
+          
+        }
+                if (flag == 1 )
+                    console.log("not prime")
+                else
+                        console.log( "is prime");
